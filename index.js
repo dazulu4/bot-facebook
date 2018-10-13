2a04343c83b3662e55bd66ce7b4c21c760a048d4
denizuqu <dzuluagaq@sura.com.co>
2018-10-13 12:45:03 -0500

    Add environment variable PAGE_ACCESS_TOKEN

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Environment variables load
+require('dotenv').config();
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -39,7 +42,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "mytoken"
+  let VERIFY_TOKEN = process.env.PAGE_ACCESS_TOKEN;
     
   // Parse the query params
   let mode = req.query['hub.mode'];
